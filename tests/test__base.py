cfcfe771f3c3921ed64f6e0b98d31f35c17a47ea
nomutin <nomura0508@icloud.com>
2023-11-02 19:55:48 +0900

    [add] add `base.py` tests.

--- tests/test__base.py
+++ tests/test__base.py
@@ -1,1 +1,99 @@
 """Tests for `distribution_extention/base.py`."""
+
+import pytest
+import torch
+
+from distribution_extention.base import DistributionBase, Independent
+from distribution_extention.continuous import Normal
+
+
+class TestIndependent:
+    """Tests for `Independent`."""
+
+    @pytest.fixture()
+    def init_dist(self) -> Normal:
+        """Initialize distribution."""
+        self.batch = 1
+        self.seq_len = 32
+        self.dim = 4
+        return Normal(
+            loc=torch.zeros(self.batch, self.seq_len, self.dim),
+            scale=torch.ones(self.batch, self.seq_len, self.dim),
+        )
+
+    def test_init(self, init_dist: Normal) -> None:
+        """Test `Independent.__init__()`."""
+        reinterpreted_batch_ndims = 1
+        dist = Independent(
+            base_distribution=init_dist,
+            reinterpreted_batch_ndims=reinterpreted_batch_ndims,
+        )
+        assert dist.reinterpreted_batch_ndims == reinterpreted_batch_ndims
+
+    def test_detach(self, init_dist: Normal) -> None:
+        """Test `Independent.detach()`."""
+        reinterpreted_batch_ndims = 1
+        dist = Independent(
+            base_distribution=init_dist,
+            reinterpreted_batch_ndims=reinterpreted_batch_ndims,
+        )
+        detached_dist = dist.detach()
+        assert isinstance(detached_dist, Independent)
+        assert detached_dist.mean.requires_grad is False
+        assert detached_dist.stddev.requires_grad is False
+
+    def test_to(self, init_dist: Normal) -> None:
+        """Test `Independent.to()`."""
+        reinterpreted_batch_ndims = 1
+        dist = Independent(
+            base_distribution=init_dist,
+            reinterpreted_batch_ndims=reinterpreted_batch_ndims,
+        )
+        device = torch.device("cpu")
+        dist = dist.to(device)
+        assert dist.mean.device == device
+        assert dist.stddev.device == device
+
+    def test__getitem__(self, init_dist: Normal) -> None:
+        """Test `Independent.__getitem__()`."""
+        reinterpreted_batch_ndims = 1
+        dist = Independent(
+            base_distribution=init_dist,
+            reinterpreted_batch_ndims=reinterpreted_batch_ndims,
+        )[:, 0, :]
+        sample = dist.sample()
+        sample_shape = torch.Size([self.batch, self.dim])
+        assert sample.shape == sample_shape
+
+    def test_squeeze(self, init_dist: Normal) -> None:
+        """Test `Independent.squeeze()`."""
+        reinterpreted_batch_ndims = 1
+        dist = Independent(
+            base_distribution=init_dist,
+            reinterpreted_batch_ndims=reinterpreted_batch_ndims,
+        )
+        dist = dist.squeeze(0)
+        expected = torch.Size([self.seq_len, self.dim])
+        assert dist.sample().shape == expected
+
+    def test_unsqueeze(self, init_dist: Normal) -> None:
+        """Test `Independent.unsqueeze()`."""
+        reinterpreted_batch_ndims = 1
+        dist = Independent(
+            base_distribution=init_dist,
+            reinterpreted_batch_ndims=reinterpreted_batch_ndims,
+        )
+        dist = dist.unsqueeze(0)
+        expected = torch.Size([1, self.batch, self.seq_len, self.dim])
+        assert dist.sample().shape == expected
+
+
+class TestDistributionBase:
+    """Tests for `DistributionBase`."""
+
+    def test_parameters(self) -> None:
+        """Test `DistributionBase.parameters()`."""
+        dist = DistributionBase()
+        with pytest.raises(NotImplementedError) as e:
+            _ = dist.parameters
+        assert e.type == NotImplementedError
